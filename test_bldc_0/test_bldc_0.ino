b397f6b7c15a182e7129b4e6103cbe5d836ddc9f
Logan Fansler <lfansler@ucsc.edu>
2020-01-12 18:53:28 -0800

    test_bldc_0 was modified to move the hub motor as well as the small bldc quad motor

--- test_bldc_0/test_bldc_0.ino
+++ test_bldc_0/test_bldc_0.ino
@@ -3,6 +3,19 @@
      by Dejan, https://howtomechatronics.com
 */
 #include <Servo.h>
+#define HM_MIN_PULSE_WIDTH_CW 1517 // microseconds
+#define HM_MAX_PULSE_WIDTH_CW 1555 // microseconds
+
+#define HM_MIN_PULSE_WIDTH_CCW 1402 // microseconds
+#define HM_MAX_PULSE_WIDTH_CCW 1363 // microseconds
+
+#define HM_MAX_CCW_SPEED 1363 // microseconds
+#define HM_MIN_CCW_SPEED 1402 // microseconds
+
+#define HM_MAX_CW_SPEED 1555 // microseconds
+#define HM_MIN_CW_SPEED 1517 // microseconds
+
+
 /*
 clockwise @ 93
 counter clockwise @ 70
@@ -12,13 +25,25 @@ int potValue;  // value from the analog pin
 void setup() {
   Serial.begin(115200);
   // Attach the ESC on pin 9
-  ESC.attach(9,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+  //ESC.attach(9,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+  ESC.attach(9,1300,1600); // (pin, min pulse width, max pulse width in microseconds) 
+//MAX CCW @ 83
+// STOP (87-93) [1421-1498]
+//MAX CW @ 100
+  
   //ESC.attach(9);
+    ESC.write(93);    // Send the signal to the ESC
+  delay(1000);
+  ESC.write(102);
+  
 }
 void loop() {
   
-  potValue = analogRead(A0);   // reads the value of the potentiometer (value between 0 and 1023)
-  potValue = map(potValue, 0, 1023, 0, 180);   // scale it to use it with the servo library (value between 0 and 180)
-  ESC.write(potValue);    // Send the signal to the ESC
+  //potValue = analogRead(A0);   // reads the value of the potentiometer (value between 0 and 1023)
+  //potValue = map(potValue, 0, 1023, 0, 180);   // scale it to use it with the servo library (value between 0 and 180)
+  //ESC.write(87);    // Send the signal to the ESC
+  //delay(10000);
+  ESC.write(101);
+  
   Serial.println(potValue);
 }
