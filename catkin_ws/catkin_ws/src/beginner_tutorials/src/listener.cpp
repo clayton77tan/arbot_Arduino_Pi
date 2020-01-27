aac98497b1b590a6eaf8d2076abca0096bf60671
clstan <clstan@ucsc.edu>
2020-01-27 15:43:00 -0800

    parce int from str C++ to file

--- catkin_ws/catkin_ws/src/beginner_tutorials/src/listener.cpp
+++ catkin_ws/catkin_ws/src/beginner_tutorials/src/listener.cpp
@@ -2,14 +2,28 @@
 #include "std_msgs/String.h"
 #include <iostream>
 #include <fstream>
+#include <sstream>
 using namespace std;
 
 void chatterCallback(const std_msgs::String::ConstPtr& msg){
 	std::fstream myfile;
 	myfile.open("/home/logan/catkin_ws/src/beginner_tutorials/launch/file.txt", std::ios_base::app);
 	ROS_INFO("I heard: [%s]", msg->data.c_str());
-	myfile << msg->data.c_str() << "\r\n";
-	//myfile.close();
+	
+	stringstream ss;
+	ss<< msg->data.c_str();
+	
+	string temp;
+	int num = 0;
+	
+	while(!ss.eof()){
+		ss>>temp;
+		if(stringstream(temp) >> num){
+			myfile << num << "\r\n";
+		}
+	}	
+	//myfile << msg->data.c_str() << "\r\n";
+	myfile.close();
 }
 
 int main(int argc, char **argv){
