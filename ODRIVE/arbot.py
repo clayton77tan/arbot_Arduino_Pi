2f658f050f73005c9bde0aecd5e1284c98d7b669
clstan <clstan@ucsc.edu>
2020-02-11 16:49:04 -0800

    need to fix motor error

--- ODRIVE/arbot.py
+++ ODRIVE/arbot.py
@@ -8,6 +8,8 @@ import odrive
 from odrive.enums import *
 import time
 import math
+import sys
+
 
 #  Function initiates the search for an Odrive. To search for a specific Odrive go into the 
 #  Odrive tool and find the serialnumber with odrv0.serial_number. Convert that value to hex
@@ -17,81 +19,80 @@ import math
 
 def search_odrive():
 	print("Searching . . . ")
-	global od, J1, J2
+	global od, J0, J1
 	od = odrive.find_any() # search for the Odrive (may take about 6 seconds)
-	J1 = od.axis0 # set J2 to axis1 to control the next motor
-	J2 = od.axis1 # set J2 to axis1 to control the next motor
+	J0 = od.axis0 # set J2 to axis1 to control the next motor
+	J1 = od.axis1 # set J2 to axis1 to control the next motor
 
 
 # The calibration() function will use the input from the user to choose to 
 # calibrate one,two, or all motors on the Odrive. 
 
 def calibration(motors):
-    if(motors == "1"):
-        J1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-        J1.motor.config.pre_calibrated = True
-        J1.encoder.config.pre_calibrated = True
-        time.sleep(15)
+	if(motors == "0"):
+		J0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+		J0.motor.config.pre_calibrated = True
+		J0.encoder.config.pre_calibrated = True
+		time.sleep(15)
 
-    if(motors == "2"):
-        J2.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-        J2.motor.config.pre_calibrated = True
-        J2.encoder.config.pre_calibrated = True
-        time.sleep(15)
+	if(motors == "1"):
+		J1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+		J1.motor.config.pre_calibrated = True
+		J1.encoder.config.pre_calibrated = True
+		time.sleep(15)
 
-    if(motors == "B"): 
-        J1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-        J1.motor.config.pre_calibrated = True
-        J1.encoder.config.pre_calibrated = True
-        time.sleep(15)
-        J2.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-        J2.motor.config.pre_calibrated = True
-        J2.encoder.config.pre_calibrated = True
-        time.sleep(15)
+	if(motors == "B"): 
+		J0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+		J0.motor.config.pre_calibrated = True
+		J0.encoder.config.pre_calibrated = True
+		time.sleep(15)
+		J1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+		J1.motor.config.pre_calibrated = True
+		J1.encoder.config.pre_calibrated = True
+		time.sleep(15)
 
 
-## This option will allow the user to input the motor (1 or 2) and the mode to execute (v or p).
+## This option will allow the user to input the motor (0 or 1) and the mode to execute (v or p).
 # Inside they will be continously asked to input the velocity of the motor or the position in
 # units of counts. 
 def control_motors(motor, mode):
-	if motor == "1":
+	if (motor == "0"):
+		J0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+		if (mode == "v"):
+			J0.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
+			while True:
+				speed = input("Set speed (counts/s) or 'q' to quit velocity mode: ")
+				if(speed == "q"):
+					J0.controller.vel_setpoint = 0
+					break
+				else:
+					J0.controller.vel_setpoint = speed
+		elif (mode == "p"):
+			J0.controller.config.control_mode = CTRL_MODE_POSITION_CONTROL
+			while True:
+				position = input("Set position (count) or 'q' to quit position mode: ")
+				if(position == "q"):
+					break
+				else:
+					J0.controller.pos_setpoint = position
+
+	elif (motor == "1"):
 		J1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-    	if (mode == "v"):
-        	J1.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
-	        while True:
-	            speed = input("Set speed (counts/s) or 'q' to quit velocity mode: ")
-	            if(speed == "q"):
-	                J1.controller.vel_setpoint = 0
-	                break
-	            else:
-	                J1.controller.vel_setpoint = speed
-    	elif (mode == "p"):
-        	J1.controller.config.control_mode = CTRL_MODE_POSITION_CONTROL
-        	while True:
-		        position = input("Set position (count) or 'q' to quit position mode: ")
-		        if(position == "q"):
-		            break
-		        else:
-		            J1.controller.pos_setpoint = position
-
-    if motor == "2":
-		J2.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-    	if (mode == "v"):
-        	J2.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
-	        while True:
-	            speed = input("Set speed (counts/s) or 'q' to quit velocity mode: ")
-	            if(speed == "q"):
-	                J2.controller.vel_setpoint = 0
-	                break
-	            else:
-	                J2.controller.vel_setpoint = speed
-    	elif (mode == "p"):
-        	J2.controller.config.control_mode = CTRL_MODE_POSITION_CONTROL
-        	while True:
-		        position = input("Set position (count) or 'q' to quit position mode: ")
-		        if(position == "q"):
-		            break
-		        else:
-		            J2.controller.pos_setpoint = position
-
+		if (mode == "v"):
+			J1.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
+			while True:
+				speed = input("Set speed (counts/s) or 'q' to quit velocity mode: ")
+				if(speed == "q"):
+					J1.controller.vel_setpoint = 0
+					break
+				else:
+					J1.controller.vel_setpoint = speed
+		elif (mode == "p"):
+			J1.controller.config.control_mode = CTRL_MODE_POSITION_CONTROL
+			while True:
+				position = input("Set position (count) or 'q' to quit position mode: ")
+				if(position == "q"):
+					break
+				else:
+					J1.controller.pos_setpoint = position
 
--- ODRIVE/odrive_control.py
+++ ODRIVE/odrive_control.py
@@ -4,11 +4,11 @@ import arbot
 arbot.search_odrive()
 
 # prompt calibrate the motor and encoder of J1
-calibrate_from_start = input("Do you want to calibrate (y or n): ")
+calibrate_from_start = input("Do you want to calibrate (0,1,B,or N): ")
 arbot.calibration(calibrate_from_start)
 
 while True:
-    motor,mode = input("Select a motor (1 or 2) and set the control mode (v or p) [Ex: 1,v]: ").split()
+    motor,mode = input("Select a motor (0 or 1) and set the control mode (v or p) [Ex: 1,v]: ").split(',')
     arbot.control_motors(motor,mode)
 
 
