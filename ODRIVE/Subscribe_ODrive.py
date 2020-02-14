2cd558ff1126dd8c2d6b39a1e0369e5abe4d7e2f
clstan <clayton77tan@gmail.com>
2020-02-13 16:55:19 -0800

    odrive_ros

--- ODRIVE/Subscribe_ODrive.py
+++ ODRIVE/Subscribe_ODrive.py
@@ -8,34 +8,36 @@ import time
 import math
 import sys
 
-def search_odrive():
+def search_odrive(): # connect to odrive
 	print("Searching . . . ")
 	global od, J0, J1
 	od = odrive.find_any() # search for the Odrive (may take about 6 seconds)
 	J0 = od.axis0 # set J2 to axis1 to control the next motor
 	J1 = od.axis1 # set J2 to axis1 to control the next motor
 
-def calibration():
+def calibration(): # calibrate encoder and motor
 	J1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
 	J1.motor.config.pre_calibrated = True
 	J1.encoder.config.pre_calibrated = True
 	time.sleep(15)
-	
-def angle_count(angle):
+
+def angle_count(angle): # angle to counts
 	count = float(angle) // -4.09
 	return count
 
-def callback(data):
+def control_motors(angle):
+	J1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+	J1.controller.config.control_mode = CTRL_MODE_TRAJECTORY_CONTROL
+	J1.controller.move_to_pos(angle_count(angle))
+
+def callback(data): # ROS receive angles and drive motor
     str1 = data.data
     listx= [int(s) for s in str1.split() if s.isdigit()]
     for inty in listx:
         angle = inty
+        
     rospy.loginfo(rospy.get_caller_id() + ' Joint Angle: %s', angle)
-    
-    J1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-	J1.controller.config.control_mode = CTRL_MODE_TRAJECTORY_CONTROL
-	J1.controller.move_to_pos(angle_count(angle))
-    
+    control_motors(angle)
 
 def listener():
 	
