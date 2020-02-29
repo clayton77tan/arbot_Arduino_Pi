5ecd07f68c0e26dc7f0ba44b25db842759e03a0e
clstan <clayton77tan@gmail.com>
2020-02-28 20:05:42 -0800

    protocall

--- ODRIVE/txt_to_odrive.py
+++ ODRIVE/txt_to_odrive.py
@@ -1,3 +1,6 @@
+import os
+import time
+
 def search_odrive(): # connect to odrive
 	print("Searching . . . ")
 	global od, J0, J1
@@ -15,26 +18,39 @@ def control_motors(angle):
 	J1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL # loop the controls
 	J1.controller.move_to_pos(angle_count(angle)) # move to the set position
 
+
 #search_odrive() # find odrive
 
 while True:
 	file1 = open("/home/arbot/arbot_Arduino_Pi/ODRIVE/file.txt", "r") # open txt file
 	file1.seek(0,0)
 	anglestr = file1.readline() # read the line of the txt file
+	if not anglestr:
+		with open("/home/arbot/arbot_Arduino_Pi/ODRIVE/file.txt", 'r') as filein:
+			text = filein.read().splitlines(True)
+		with open("/home/arbot/arbot_Arduino_Pi/ODRIVE/file.txt", 'w') as fileout:
+			fileout.writelines(text[1:])
+		time.sleep(0.1)
+		continue
+
+	if anglestr:
+		with open("/home/arbot/arbot_Arduino_Pi/ODRIVE/file.txt", 'r') as filein:
+			text = filein.read().splitlines(True)
+		with open("/home/arbot/arbot_Arduino_Pi/ODRIVE/file.txt", 'w') as fileout:
+			fileout.writelines(text[1:])
+		
+		anglelist = anglestr.split(',') # split the string into a list
+		angle0 = float(anglelist[0]) # split list into a float
+		angle1 = float(anglelist[1]) # split list into a float
+		angle2 = float(anglelist[2]) # split list into a float
+		angle3 = float(anglelist[3]) # split list into a float
+		print(str(angle0) + "\n")
+		print(str(angle1) + "\n")
+		print(str(angle2) + "\n")
+		print(str(angle3) + "\n")
+
+				
 	
-	with open("/home/arbot/arbot_Arduino_Pi/ODRIVE/file.txt", 'r') as filein:
-		text = filein.read().splitlines(True)
-	with open("/home/arbot/arbot_Arduino_Pi/ODRIVE/file.txt", 'w') as fileout:
-		fileout.writelines(text[1:])
-	
-	anglelist = anglestr.split(',') # split the string into a list
-	angle0 = float(anglelist[0]) # split list into a float
-	angle1 = float(anglelist[1]) # split list into a float
-	angle2 = float(anglelist[2]) # split list into a float
-	angle3 = float(anglelist[3]) # split list into a float
-	print(str(angle0) + "\n")
-	print(str(angle1) + "\n")
-	print(str(angle2) + "\n")
-	print(str(angle3) + "\n")
+		# file1.close()
 	
 	#control_motors(angle1) # control motors
