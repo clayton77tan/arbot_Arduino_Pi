2c4726f57282f86182581992f70547e85e1862a3
clstan <clayton77tan@gmail.com>
2020-02-29 16:06:00 -0800

    script ready, need to finish odrivetune

--- ODRIVE/txt_to_odrive.py
+++ ODRIVE/txt_to_odrive.py
@@ -1,5 +1,11 @@
 import os
+
+import odrive
+from odrive.enums import *
 import time
+import math
+import sys
+
 
 def search_odrive(): # connect to odrive
 	print("Searching . . . ")
@@ -18,9 +24,23 @@ def control_motors(angle):
 	J1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL # loop the controls
 	J1.controller.move_to_pos(angle_count(angle)) # move to the set position
 
+def calibration():
+	J1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+	J1.motor.config.pre_calibrated = True
+	J1.encoder.config.pre_calibrated = True
+	time.sleep(15)
 
-#search_odrive() # find odrive
+def shut_down():
+	J0.controller.config.control_mode = CTRL_MODE_TRAJECTORY_CONTROL
+	J1.controller.config.control_mode = CTRL_MODE_TRAJECTORY_CONTROL
+	J0.controller.move_to_pos(0)
+	J1.controller.move_to_pos(0)
+	time.sleep(3)
+	sys.exit(0)
 
+# search_odrive() # find odrive
+# calibration()
+	
 while True:
 	file2 = open("/home/arbot/arbot_Arduino_Pi/ODRIVE/protocol.txt", "r")
 	file2.seek(0,0)
@@ -60,12 +80,20 @@ while True:
 			angle2 = float(anglelist[2]) # split list into a float
 			angle3 = float(anglelist[3]) # split list into a float
 			
+			# degree0 = (float(angle0 * 180)) // 3.14
+			# degree1 = (float(angle1 * 180)) // 3.14
+			# degree2 = (float(angle2 * 180)) // 3.14
+			# degree3 = (float(angle3 * 180)) // 3.14
+			
 			print(str(angle0) + "\n")
 			print(str(angle1) + "\n")
 			print(str(angle2) + "\n")
 			print(str(angle3) + "\n")
-				
 	
-		# file1.close()
+		# switch = input()
+		# if(switch != "q"):
+			# control_motors(angle0) # control motors
+		# elif(switch == "q"):
+			# shut_down()
 	
-	#control_motors(angle1) # control motors
+	
