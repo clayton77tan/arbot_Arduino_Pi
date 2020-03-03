5b827996eef6fca84417c66841f8e9612787da0f
Clayton Tan <clstan@ucsc.edu>
2020-03-03 02:08:14 -0800

    print index

--- ODRIVE/ISRODrive.py
+++ ODRIVE/ISRODrive.py
@@ -1,9 +1,9 @@
 import os
 from array import *
-from numpy import array
-from numpy import empty
-import odrive
-from odrive.enums import *
+# from numpy import array
+# from numpy import empty
+# import odrive
+# from odrive.enums import *
 import time
 import math
 import sys
@@ -11,21 +11,21 @@ import threading
 
 angle = []
 time = []
+index = 0
 
 while True:
-	file1 = open("/home/arbot/arbot_Arduino_Pi/ODRIVE/angle.txt", "r") # open txt file
+	file1 = open("/home/clstan/arbot_Arduino_Pi/ODRIVE/angle.txt", "r") # open txt file
 	file1.seek(0,0)
 	anglestr = file1.readline() # read the line of the txt file
 	
 	anglelist = anglestr.split(',') # split the string into a list
-	
-
 
 	if(anglelist[0]):
 		angle.append(anglelist)
-		print(angle[1])
+		print(angle[index])
 
-	with open("/home/arbot/arbot_Arduino_Pi/ODRIVE/angle.txt", 'r') as filein1:
+	with open("/home/clstan/arbot_Arduino_Pi/ODRIVE/angle.txt", 'r') as filein1:
 		text = filein1.read().splitlines(True)
-	with open("/home/arbot/arbot_Arduino_Pi/ODRIVE/angle.txt", 'w') as fileout1:
+	with open("/home/clstan/arbot_Arduino_Pi/ODRIVE/angle.txt", 'w') as fileout1:
 		fileout1.writelines(text[1:])
+	index = index + 1
