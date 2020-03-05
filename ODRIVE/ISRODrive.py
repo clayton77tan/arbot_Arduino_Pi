893dfc39a56ab2a9b67ab20d00a157c575f6f0e3
clstan <clayton77tan@gmail.com>
2020-03-04 16:39:02 -0800

    start process angles

--- ODRIVE/ISRODrive.py
+++ ODRIVE/ISRODrive.py
@@ -9,44 +9,60 @@ import math
 import sys
 import threading
 
-
+# intialize variables
 angle = []
 time = []
 index = 0
 timeIndex = 0
+processedTime = []
 
-
+# fucntion for subtracting 1 sec from time
 def process_time(staticTime):
 	dynamicTime = [(-1 + sec) for sec in staticTime]
 	return dynamicTime
 
+
+
+processedTime.clear() # clear the processedtime list
+time.clear() # clear the time list
+angle.clear() # clear the angle list
+
 while True:
 	text = open("/home/arbot/arbot_Arduino_Pi/ODRIVE/angle.txt", "r") # open txt file
-	text.seek(0,0)
+	text.seek(0,0) # start at beginning of text file
 	textstr = text.readline() # read the line of the txt file
 	
 	textlist = textstr.split(',') # split the string into a list
 
-	if(textlist[0]):
-		angle.append(textlist[0:4])
-		# print("angle: " + str(angle[index]))
-		time.append(textlist[4:5])
-		# print("time: " + str(time[index]))
-		index = index + 1
+	if(textlist[0]): # check if textlist is empty or not
+		angle.append(textlist[0:4]) # append to angle list
+		time.append(textlist[4:5]) # append to time list
+		index = index + 1 # counter for each line read
 	
-	timelist = [''.join(line) for line in time]
-	newtime = [new[:-1] for new in timelist]
-	listtime = [int(x) for x in newtime]
+	timelist = [''.join(line) for line in time] # make list of list into one list
+	newtime = [new[:-1] for new in timelist] # get rid of \n
+	listtime = [int(x) for x in newtime] # make list items from str to int
 	
-	delay = threading.Event()
-	delay.wait(1)
-	dynamicTime = process_time(listtime)
-	
-	
-	
-	# if (dynamicTime[timeIndex] == 0):
+	if(math.fmod(index,20) == 0): # check if script has read 20 lines from txt file
+		delay = threading.Event() # clear a dummy event
+		delay.wait(1) # 1 sec delay
+		
+		if(processedTime): # check if processedTime is empty or not
+			listtime[0:len(processedTime)] = processedTime[0:len(processedTime)] # overwrite listtime with old listtime since each element has been subtracted by 1
+
+		processedTime = process_time(listtime) # call function to subtract time by 1
+		if(processedTime and processedTime[0:20] == ([0]*20)): # check if time has counted down to zero or not
+			processedTime = processedTime[20:] # pop the first 20 values off the processedTime list
+			time = time[20:] # pop the first 20 values off the time list to ensure it doesn't rewrite ther values again
+			
+			# process the first 20 joint angles
+			
+			
+		print("processed: " + str(processedTime))
+	print("list: " + str(listtime))
 		
 
+	# read the entire text file to pop the current line off the text file
 	with open("/home/arbot/arbot_Arduino_Pi/ODRIVE/angle.txt", 'r') as filein1:
 		text = filein1.read().splitlines(True)
 	with open("/home/arbot/arbot_Arduino_Pi/ODRIVE/angle.txt", 'w') as fileout1:
--- ODRIVE/ISR_angle.py
+++ ODRIVE/ISR_angle.py
@@ -2,21 +2,23 @@ import math
 import time
 import threading
 
+# intialize variables
 count = 1
 file1 = open("/home/arbot/arbot_Arduino_Pi/ODRIVE/angle.txt", "w")
 file1.seek(0,0)
 flag = 10000
+incre = 1
 
 while True:
-	while(count <= 15):
+	while(count <= 100): # count to 100
 		if(math.fmod(count,5) != 0):
-			file1.write(str(count) + ",")
+			file1.write(str(count) + ",") # separate by ','
 		elif(math.fmod(count,5) == 0):
-			file1.write(str(count) + "\n")
-		count = count + 1
-	if(count > 15):
-		while(flag > 1):
+			file1.write(str(5 + incre) + "\n") # separate by '\n' every 5th number
+		count = count + 1 # increment the count
+	if(count > 100): # check if the count has reached 100
+		incre = incre + 1 # increment time
+		while(flag > 1): # blocking delay to reset count back to 1
 			flag = flag - 1
-		count = 1
-		flag = 10000
-		# break
+		count = 1 # reset count to 1
+		flag = 10000 # reset the flag
