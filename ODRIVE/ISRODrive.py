fb283f0c1c752f41ac3839cd646bdcecb2112a00
clstan <clayton77tan@gmail.com>
2020-03-04 20:44:24 -0800

    fixed loop

--- ODRIVE/ISRODrive.py
+++ ODRIVE/ISRODrive.py
@@ -17,6 +17,8 @@ timeIndex = 0
 processedTime = []
 angleIndex = 0
 angleODrive = 0
+processcount = 0
+
 
 
 # fucntion for subtracting 1 sec from time
@@ -34,7 +36,6 @@ while True:
 	text = open("/home/arbot/arbot_Arduino_Pi/ODRIVE/angle.txt", "r") # open txt file
 	text.seek(0,0) # start at beginning of text file
 	textstr = text.readline() # read the line of the txt file
-	
 	textlist = textstr.split(',') # split the string into a list
 
 	if(textlist[0] and textlist[0] != '\n'): # check if textlist is empty or not
@@ -42,36 +43,47 @@ while True:
 		listtext = [int(z) for z in txt]
 		angle.append(listtext) # append to angle list
 		time.append(textlist[4:5]) # append to time list
-		index = index + 1 # counter for each line read
+		
+		# read the entire text file to pop the current line off the text file
+		with open("/home/arbot/arbot_Arduino_Pi/ODRIVE/angle.txt", 'r') as filein1:
+			text = filein1.read().splitlines(True)
+		with open("/home/arbot/arbot_Arduino_Pi/ODRIVE/angle.txt", 'w') as fileout1:
+			fileout1.writelines(text[1:])
+		
+	index = index + 1 # counter for each line read
 	
 	timelist = [''.join(line) for line in time] # make list of list into one list
 	newtime = [new[:-1] for new in timelist] # get rid of \n
 	listtime = [int(x) for x in newtime] # make list items from str to int
-	
-	
+
+
 	if(math.fmod(index,20) == 0): # check if script has read 20 lines from txt file
+		print("in mod")
 		delay = threading.Event() # clear a dummy event
 		delay.wait(1) # 1 sec delay
 		
-		if(processedTime): # check if processedTime is empty or not
+		if(processcount > 0): # check if processedTime is empty or not
 			listtime[0:len(processedTime)] = processedTime[0:len(processedTime)] # overwrite listtime with old listtime since each element has been subtracted by 1
-
-		processedTime = process_time(listtime) # call function to subtract time by 1
-		if(processedTime and processedTime[0:20] == ([0]*20)): # check if time has counted down to zero or not
+		processcount = processcount + 1
+		
+		if(listtime[0:20] != ([0]*20)):
+			processedTime = process_time(listtime) # call function to subtract time by 1
+		
+		print(processedTime)
+		if(processedTime[0:20] == ([0]*20)): # check if time has counted down to zero or not
 			processedTime = processedTime[20:] # pop the first 20 values off the processedTime list
 			time = time[20:] # pop the first 20 values off the time list to ensure it doesn't rewrite ther values again
-			
 			# process the first 20 joint angles
 			while(angleODrive < 20):
-				print(angle[0][:])
+				print(str(angle[0][0]) + ',' +str(angle[0][1]) + ',' +str(angle[0][2]) + ',' +str(angle[0][3]))
+				# print(str(angle[0][0]))
+				# print(str(angle[0][1]))
+				# print(str(angle[0][2]))
+				# print(str(angle[0][3]))
+				# print('\n')
 				angle = angle[1:][:]
 				angleODrive = angleODrive + 1
+			if(angleODrive == 20):
+				angle = angle[1:][:]
 			angleODrive = 0
-		
-
-	# read the entire text file to pop the current line off the text file
-	with open("/home/arbot/arbot_Arduino_Pi/ODRIVE/angle.txt", 'r') as filein1:
-		text = filein1.read().splitlines(True)
-	with open("/home/arbot/arbot_Arduino_Pi/ODRIVE/angle.txt", 'w') as fileout1:
-		fileout1.writelines(text[1:])
 	
--- ODRIVE/ISR_angle.py
+++ ODRIVE/ISR_angle.py
@@ -3,22 +3,23 @@ import time
 import threading
 
 # intialize variables
-count = 1
+count = 201
 file1 = open("/home/arbot/arbot_Arduino_Pi/ODRIVE/angle.txt", "w")
 file1.seek(0,0)
 flag = 10000
 incre = 1
 
 while True:
-	while(count <= 100): # count to 100
+	while(count <= 300): # count to 100
 		if(math.fmod(count,5) != 0):
 			file1.write(str(count) + ",") # separate by ','
 		elif(math.fmod(count,5) == 0):
 			file1.write(str(5 + incre) + "\n") # separate by '\n' every 5th number
 		count = count + 1 # increment the count
-	if(count > 100): # check if the count has reached 100
+	if(count > 300): # check if the count has reached 100
 		incre = incre + 1 # increment time
-		while(flag > 1): # blocking delay to reset count back to 1
-			flag = flag - 1
-		count = 1 # reset count to 1
-		flag = 10000 # reset the flag
+		# while(flag > 1): # blocking delay to reset count back to 1
+			# flag = flag - 1
+		# count = 1 # reset count to 1
+		# flag = 10000 # reset the flag
+		break
